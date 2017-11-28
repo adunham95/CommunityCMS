5879f16e668099f83a12d17e636c8e3c4eaad1e2
Adrian <adunham95@gmail.cooom>
2017-11-27 21:15:40 -0500

    Added routing

--- webview/src/app/app.module.ts
+++ webview/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router"
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  // {path: 'register', component: RegisterComponent},
+  // {path: 'login', component: LoginComponent},
+  // {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  // {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
