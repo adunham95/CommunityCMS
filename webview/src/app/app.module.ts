552ffef02cc0c0f0b103564535ca9c18cb47022f
Adrian <adunham95@gmail.cooom>
2017-11-28 19:31:23 -0500

    Added login component

--- webview/src/app/app.module.ts
+++ webview/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router"
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule} from "@angular/material";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   // {path: 'register', component: RegisterComponent},
-  // {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
   // {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   // {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
 ];
@@ -19,11 +21,16 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
+    DashboardComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
