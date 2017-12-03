f92ee969fdfb63008509b9d4980670d2e27ef073
Adrian <adunham95@gmail.cooom>
2017-12-03 13:08:31 -0500

    Added profile card

---
+++ webview/src/app/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'profile-card',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
