d793948c59fb7c169adeab4303854a928984f2df
Adrian <adunham95@gmail.cooom>
2017-12-05 17:12:57 -0500

    Spelling change

--- api/src/main/java/com/skyline/api/CommunityRepository.java
+++ api/src/main/java/com/skyline/api/CommunityRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CommunityRepository extends MongoRepository<Community, String> {
     public Community findCommunitiesByName(String name);
-    public Community findCommunitiesById(String id);
+    public Community findCommunitiesByid(String id);
 }
